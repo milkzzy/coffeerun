bd1b3c11f3a8819f6e18dc8418d57d760fa2c6c1
milkzzy <jackmisdom@gmail.com>
2020-05-01 11:21:12 -0400

    Organization and Routing

---
+++ src/components/PendingOrders.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Card,
+    CardBody,
+    CardHeader,
+} from 'reactstrap';
+
+function PendingOrders() {
+    return (
+        <Card>
+            <CardHeader>Pending Orders</CardHeader>
+            <CardBody>
+                Pending Orders Will Be Here
+            </CardBody>
+        </Card>
+    );
+}
+
+export default PendingOrders;
